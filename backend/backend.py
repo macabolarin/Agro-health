9e9f095f144c971581e099375d301cde9bba7a9b
macabolarin <macabolarin@gmail.com>
2026-03-04 14:28:29 +0100

    add python file

---
+++ backend/backend.py
@@ -0,0 +1,43 @@
+virus_treatment = {
+    "influenza": {
+        "symptoms": ["fever", "cough", "sore throat", "headache"],
+        "treatment": ["rest", "hydration", "antiviral medication"]
+    },
+    "common cold": {
+        "symptoms": ["runnynose", "cough", "shivering", "feather dropping", "running temprature" "headache"],
+        "treatment": ["rest", "antibiotics", "increase temperature but not above 28 degree"]
+    },
+    "Coccidiosis": {
+        "symptoms": ["fever", "cough", "shortness of breath", "high mortality", "brownish watery stool"],
+        "treatment": [ "hydration", "antiviral medication", "spacing"]
+    },
+    "pox": {
+        "symptoms": ["swollen around eyes", "nose running", "shivering and feather droppings"],
+        "treatment": ["antipox medications", "antibiotics"]
+    },
+    "cholera and fever": {
+        "symptoms": ["continous stool", "cough", "shivering and feather dropping in birds and weakness in other"],
+        "treatment": ["increase hygiene", "cholera and fever medicatioons"]
+    },
+    }
+
+def get_virus_treatment(symptoms):
+    for virus, info in virus_treatment.items():
+        if all(symptom in info["symptoms"] for symptom in symptoms):
+            return virus, info["treatment"]
+    return None, None
+def main():
+    print("Enter your symptoms (seperated by commas):")
+    symptoms = [symptoms.strip() for symptom in input().split(",")]
+    virus, treatment = get_virus_treatment(symptoms)
+    if virus:
+        print(f"possible virus: {virus}")
+        print(f"recommended treatment:")
+        for item in treatment:
+            print(f"- {item}")
+    else:
+        print("no matching virus found. please consult a doctor.")
+
+    
+    
+            
